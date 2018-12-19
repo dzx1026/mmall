b00213823d936c99f8cd8f46603e2253e840fe00
dzx <754958439@qq.com>
2018-12-19 21:45:19 +0800

    write login

---
+++ src/main/java/com/mmall/controller/portal/UserController.java
@@ -0,0 +1,30 @@
+package com.mmall.controller.portal;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpSession;
+
+/**
+ * 前台用户类
+ */
+@Controller
+@RequestMapping("/user/")   //当前类的所有方法的请求地址都在/user/下
+public class UserController {
+    /**
+     *
+     * @param username
+     * @param password
+     * @param session
+     * @return
+     */
+    @RequestMapping("login")
+    @ResponseBody                    //这个注解用来实现json序列化
+    public Object login(String username, String password, HttpSession session){
+
+
+
+        return null;
+    }
+}
